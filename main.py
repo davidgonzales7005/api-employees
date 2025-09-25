dcdc2d9e4ef0b40479b247ac7639706579f40853
davidgonzales7005 <david.gonzales@utec.edu.pe>
2025-09-24 19:02:15 -0500

    main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-host_name = "107.20.73.75" # IPv4 privada de "MV Bases de Datos"
+host_name = "172.31.30.169" # IPv4 privada de "MV Bases de Datos"
 port_number = "8005"
 user_name = "root"
 password_db = "utec"
@@ -89,3 +89,4 @@ def delete_employee(id: int):
     mydb.close()
 
     return {"message": "Employee deleted successfully"}
+
